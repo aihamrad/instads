de9374d5725de2be2790e5be206eac6bce2478d3
aihamrad <aiham.radwan@hotmail.com>
2022-01-18 08:43:51 +0400

    setup assets folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './assets/main.scss';
+import Home from './routes/Home'
+import Navbar from './routes/Navbar';
+import data from "./json/users.json"
+import Login from './routes/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {  
+  constructor(props) {
+    super(props)
+    this.state = {
+      numberOfItems: 0,
+      showCheckout: false,
+      selectedUser:''
+    }
+  }
+
+  handleSelectedItems = (numberOfItems) => {
+    this.setState({
+      numberOfItems
+    })
+  }
+
+  handleCheckoutView = () => {
+    this.setState((prevState) => ({
+      showCheckout: !prevState.showCheckout
+    }))
+  }
+
+  handleHomePageShow = () => {
+    this.setState({
+      showCheckout: false
+    })
+  }
+
+  handleUser = (user) => {
+    if (user === '') {
+      window.location.reload();
+    }
+    this.setState({
+      selectedUser: user
+    })
+  }
+
+  render() {
+    const {numberOfItems, showCheckout, selectedUser} = this.state
+    return (
+      <div>  
+        <Navbar selectedUser={selectedUser} numberOfItems={numberOfItems} handleUser={this.handleUser} users={data.users} showCheckout={showCheckout} handleCheckoutView={this.handleCheckoutView} />
+        <div className="container cursor-default">
+        {selectedUser && <Home selectedUser={selectedUser} handleSelectedItems={this.handleSelectedItems} showCheckout={showCheckout} handleCheckoutView={this.handleCheckoutView} handleHomePageShow={this.handleHomePageShow} />}
+        {!selectedUser && <Login users={data.users} handleUser={this.handleUser}/>}     
+        </div>
+      </div>  
+    )
+  }};
 
 export default App;
